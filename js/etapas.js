8f62c81a4d2f1bc8399dea8e92cff3de6677dce8
Elves Queiroz <elves.waq@gmail.com>
2021-10-29 22:00:17 -0300

    projeto urna eletronica

---
+++ js/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Carlos do Carão',
+                partido: 'KBC',
+                fotos: [
+                    {url: '38111.jpg',legenda: 'Vereador', small:false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Marcia da Terra',
+                partido: 'PLT',
+                fotos: [
+                    {url: '77222.jpg',legenda: 'Vereador', small:false}
+                ]
+
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99' ,
+                nome: 'João Braz',
+                partido: 'KBC',
+                vice: 'Pedro Vaz',
+                fotos:[
+                    {url:'99.jpg',legenda:'Prefeito'},
+                    {url:'99_2.jpg',legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '84' ,
+                nome: 'Irineu Neto',
+                partido: 'PLT',
+                vice: 'Donato dias',
+                fotos:[
+                    {url:'84.jpg',legenda:'Prefeito'},
+                    {url:'84_2.jpg',legenda:'Vice-Prefeito', small:true}
+                ]   
+            },
+            
+        ]
+    }
+
+];
